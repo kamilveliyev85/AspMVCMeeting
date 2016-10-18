1b633ba97b3d73eb33f8d1a13b88fe9d1039597f
Kamil.Veliyev <Kamil.Veliyev@NAZHQ002SD115>
2016-10-18 17:26:04 +0400

    resx

--- AspMVCMeeting/App_GlobalResources/Global.Designer.cs
+++ AspMVCMeeting/App_GlobalResources/Global.Designer.cs
@@ -61,11 +61,371 @@ namespace AspMVCMeeting.App_GlobalResources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Tarih.
+        /// </summary>
+        public static string LBL_MT_DATE {
+            get {
+                return ResourceManager.GetString("LBL_MT_DATE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Açıklama.
+        /// </summary>
+        public static string LBL_MT_DESCIPTION {
+            get {
+                return ResourceManager.GetString("LBL_MT_DESCIPTION", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Bitme Saati.
+        /// </summary>
+        public static string LBL_MT_FINISH_TIME {
+            get {
+                return ResourceManager.GetString("LBL_MT_FINISH_TIME", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Takip Eden.
+        /// </summary>
+        public static string LBL_MT_FOLLOWER_USERID {
+            get {
+                return ResourceManager.GetString("LBL_MT_FOLLOWER_USERID", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Toplantı Yonetici.
+        /// </summary>
+        public static string LBL_MT_MANAGER {
+            get {
+                return ResourceManager.GetString("LBL_MT_MANAGER", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to No:.
+        /// </summary>
+        public static string LBL_MT_NO {
+            get {
+                return ResourceManager.GetString("LBL_MT_NO", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Yer.
+        /// </summary>
+        public static string LBL_MT_PLACE {
+            get {
+                return ResourceManager.GetString("LBL_MT_PLACE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Özel Kod 1.
+        /// </summary>
+        public static string LBL_MT_SCODE1 {
+            get {
+                return ResourceManager.GetString("LBL_MT_SCODE1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Özel Kod 2.
+        /// </summary>
+        public static string LBL_MT_SCODE2 {
+            get {
+                return ResourceManager.GetString("LBL_MT_SCODE2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Özel Kod 3.
+        /// </summary>
+        public static string LBL_MT_SCODE3 {
+            get {
+                return ResourceManager.GetString("LBL_MT_SCODE3", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Özel Kod 4.
+        /// </summary>
+        public static string LBL_MT_SCODE4 {
+            get {
+                return ResourceManager.GetString("LBL_MT_SCODE4", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Özel Kod 5.
+        /// </summary>
+        public static string LBL_MT_SCODE5 {
+            get {
+                return ResourceManager.GetString("LBL_MT_SCODE5", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Başlama Saati.
+        /// </summary>
+        public static string LBL_MT_START_TIME {
+            get {
+                return ResourceManager.GetString("LBL_MT_START_TIME", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Durum.
+        /// </summary>
+        public static string LBL_MT_STS {
+            get {
+                return ResourceManager.GetString("LBL_MT_STS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Taglar.
+        /// </summary>
+        public static string LBL_MT_TAGS {
+            get {
+                return ResourceManager.GetString("LBL_MT_TAGS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Konu.
+        /// </summary>
+        public static string LBL_MT_TITLE {
+            get {
+                return ResourceManager.GetString("LBL_MT_TITLE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Toplanti Turu.
+        /// </summary>
+        public static string LBL_MT_TYPE {
+            get {
+                return ResourceManager.GetString("LBL_MT_TYPE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Bilgilendirilecekler.
+        /// </summary>
+        public static string LBL_MT_USER_CC {
+            get {
+                return ResourceManager.GetString("LBL_MT_USER_CC", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Kurum Dışı Katılımcılar.
+        /// </summary>
+        public static string LBL_MT_USER_OUT {
+            get {
+                return ResourceManager.GetString("LBL_MT_USER_OUT", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Katılımcılar.
+        /// </summary>
+        public static string LBL_MT_USER_PARTICIPANTS {
+            get {
+                return ResourceManager.GetString("LBL_MT_USER_PARTICIPANTS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Teyid Eden.
+        /// </summary>
+        public static string LBL_MTL_CONFIRMER {
+            get {
+                return ResourceManager.GetString("LBL_MTL_CONFIRMER", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Karar Türü.
+        /// </summary>
+        public static string LBL_MTL_DECISION_TYPE {
+            get {
+                return ResourceManager.GetString("LBL_MTL_DECISION_TYPE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Departman.
+        /// </summary>
+        public static string LBL_MTL_DEPARTMENT {
+            get {
+                return ResourceManager.GetString("LBL_MTL_DEPARTMENT", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Yönetici.
+        /// </summary>
+        public static string LBL_MTL_EXECUTANT {
+            get {
+                return ResourceManager.GetString("LBL_MTL_EXECUTANT", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Bitme Tarihi.
+        /// </summary>
+        public static string LBL_MTL_FINISH_DATE {
+            get {
+                return ResourceManager.GetString("LBL_MTL_FINISH_DATE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Teklif Uygun mu.
+        /// </summary>
+        public static string LBL_MTL_OFFER_OK {
+            get {
+                return ResourceManager.GetString("LBL_MTL_OFFER_OK", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Teklif Eden.
+        /// </summary>
+        public static string LBL_MTL_OFFER_USER {
+            get {
+                return ResourceManager.GetString("LBL_MTL_OFFER_USER", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Proje Kodu.
+        /// </summary>
+        public static string LBL_MTL_PROJECT_CODE {
+            get {
+                return ResourceManager.GetString("LBL_MTL_PROJECT_CODE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Bağlı Oldugu Form Açıklama.
+        /// </summary>
+        public static string LBL_MTL_RELATED_FORM_DESCRIPTION {
+            get {
+                return ResourceManager.GetString("LBL_MTL_RELATED_FORM_DESCRIPTION", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Bağlı Olduğu Form.
+        /// </summary>
+        public static string LBL_MTL_RELATED_FORM_REF {
+            get {
+                return ResourceManager.GetString("LBL_MTL_RELATED_FORM_REF", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Sorumlu.
+        /// </summary>
+        public static string LBL_MTL_RESPONSIBLE {
+            get {
+                return ResourceManager.GetString("LBL_MTL_RESPONSIBLE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Başlama Tarihi.
+        /// </summary>
+        public static string LBL_MTL_START_DATE {
+            get {
+                return ResourceManager.GetString("LBL_MTL_START_DATE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Kayıt Türü.
+        /// </summary>
+        public static string LBL_MTL_TYPE {
+            get {
+                return ResourceManager.GetString("LBL_MTL_TYPE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Bütçe.
+        /// </summary>
+        public static string LBL_PRJ_BUDGET {
+            get {
+                return ResourceManager.GetString("LBL_PRJ_BUDGET", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Para Birimi.
+        /// </summary>
+        public static string LBL_PRJ_BUDGET_CURRENCY {
+            get {
+                return ResourceManager.GetString("LBL_PRJ_BUDGET_CURRENCY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Project code.
         /// </summary>
         public static string LBL_PRJ_CODE {
             get {
                 return ResourceManager.GetString("LBL_PRJ_CODE", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Adam/Gün sayı.
+        /// </summary>
+        public static string LBL_PRJ_MAN_DAY {
+            get {
+                return ResourceManager.GetString("LBL_PRJ_MAN_DAY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Üyeler.
+        /// </summary>
+        public static string LBL_PRJ_MEMBERS {
+            get {
+                return ResourceManager.GetString("LBL_PRJ_MEMBERS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Proje Adı.
+        /// </summary>
+        public static string LBL_PRJ_NAME {
+            get {
+                return ResourceManager.GetString("LBL_PRJ_NAME", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Proje Durumu.
+        /// </summary>
+        public static string LBL_PRJ_STS {
+            get {
+                return ResourceManager.GetString("LBL_PRJ_STS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Versiyon.
+        /// </summary>
+        public static string VERSION {
+            get {
+                return ResourceManager.GetString("VERSION", resourceCulture);
             }
         }
     }
